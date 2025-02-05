906eea1cdff9ea44b5f5b208eac53c2a8543dca1
ulad_bars <vbaranovski@gmail.com>
2025-02-05 21:53:58 +0000

    Remove .ci/DevopsPath from Git tracking

--- .ci/builder.sh
+++ .ci/builder.sh
@@ -1,12 +1,32 @@
 #! bin/bash
-cd ~/
+
 echo “Creating Repository folder”
+
 mkdir Repositories || cd Repositories
+
 echo “Cloning Repo if not exist, or pulling latest changes“
-git clone https://github.com/VBaranouski/DevopsPath.git
-git clone https://github.com/VBaranouski/DevopsPath.git || cd DevopsPath/ || git pull
-cd homepage/
-Echo “Building docker image”
+git clone https://github.com/VBaranouski/DevopsPath.git || cd DevopsPath/ || git pull  
+
+echo '<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SDL</title>
+</head>
+<body>
+    <h1>Привет SDL:))</h1>
+</body>
+</html>' > ApacheHomepage.html
+
+git add -A && git commit -m “adding homepage” && git push origin main
+
+echo “Building docker image”
 docker build -t dockerhellosdl .
+
 echo “Running docker container”
 docker run -d -p 8090:80 dockerhellosdl
+
+
+
+
+
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu/apache2:2.4-24.10_edge
-COPY helloSDL.html /var/www/html/index.html
+COPY ApacheHomepage.html /var/www/html/index.html
 ENTRYPOINT ["apache2ctl", "-D", "FOREGROUND"]
 EXPOSE 80
